698d8adf2256aa1a227a55e69bdd9eb3430d7605
AlexMFV <alexmfv1@gmail.com>
2019-12-20 15:23:01 +0000

    Added css

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
-app.get('/faceitstats/', function(req, res){
+app.get('/', function(req, res){
   res.render('index');
 });
 
