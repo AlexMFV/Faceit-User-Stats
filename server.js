8b0f7555650d250500602370073e3e490b9e463f
AlexMFV <alexmfv1@gmail.com>
2019-12-20 02:30:09 +0000

    Updated project to Node App

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs');
+const pg = require('pg');
+
+//Set the visible folders for the server
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+//app.use(express.json());
+
+//Set the engine
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(8080);
+console.log("Server listening on port 8080!");
