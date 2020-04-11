0d841763b745a101584a6c427bc0285329857b65
Bernardo Caeiro <44115914+BernardoCaeiro@users.noreply.github.com>
2020-04-11 01:00:34 +0100

    More changes at Match.html
    Added Sidebar menu, ad space and colores adjusts

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,28 @@
 //Adding event handlers
-let btnSearch = document.getElementById('btnSearch');
-btnSearch.addEventListener('click', getUserData);
-
-let faceitUser = document.getElementById('steamId');
-faceitUser.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13 && faceitUser.value != "") {
-    event.preventDefault();
-    getUserData();
+$(document).ready(function(){
+  let btnSearch = document.getElementById('btnSearch');
+  if(btnSearch != null)  {
+    btnSearch.addEventListener('click', getUserData);
   }
-  else
-    if(faceitUser.value != ""){
-      //Menssage there is no FaceitUser written
+
+  let faceitUser = document.getElementById('steamId');
+  faceitUser.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13 && faceitUser.value != "") {
+      event.preventDefault();
+      getUserData();
     }
+    else
+      if(faceitUser.value != ""){
+        //Menssage there is no FaceitUser written
+      }
+  });
 });
 
 function getUserData() {
   const user = document.getElementById('steamId').value;
   const game = getRadioButtonValue();
   if(user != ""){
-    window.location.href = '/player.html?id=' + user + '&game=' + game; //"/player.html?id=" + user;
+    window.location.href = '/player.html?id=' + user + '&game=' + "csgo"; //"/player.html?id=" + user;
   }
   else {
     //Menssage there is no FaceitUser written
