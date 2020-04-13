0bab6b421616824b567fe6901f55e0bc27dfc868
Bernardo Caeiro <44115914+BernardoCaeiro@users.noreply.github.com>
2020-04-13 16:55:48 +0100

    Match and Index changes
    Match finished (only level left) and index top's lefting

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,23 @@
 //Adding event handlers
-$(document).ready(function(){
+window.addEventListener('DOMContentLoaded', async function(){
+  initFuncs();
+});
+
+function initFuncs(){
+  btnSearchClick();
+  faceitUserAtributes();
+
+  createElements();
+}
+
+function btnSearchClick(){
   let btnSearch = document.getElementById('btnSearch');
   if(btnSearch != null)  {
     btnSearch.addEventListener('click', getUserData);
   }
+}
 
+function faceitUserAtributes(){
   let faceitUser = document.getElementById('steamId');
   faceitUser.addEventListener("keyup", function(event) {
     if (event.keyCode === 13 && faceitUser.value != "") {
@@ -16,11 +29,11 @@ $(document).ready(function(){
         //Menssage there is no FaceitUser written
       }
   });
-});
+}
 
 function getUserData() {
   const user = document.getElementById('steamId').value;
-  const game = getRadioButtonValue();
+  //const game = getRadioButtonValue();
   if(user != ""){
     window.location.href = '/player.html?id=' + user + '&game=' + "csgo"; //"/player.html?id=" + user;
   }
@@ -28,8 +41,103 @@ function getUserData() {
     //Menssage there is no FaceitUser written
   }
 }
-
 function getRadioButtonValue(){
   const children = document.querySelector(".radio:checked").value;
   return children;
+}
+
+async function createElements(){
+  const col = [];
+
+  col.push(createTop5Elo());
+  col.push(createTop5Kd());
+
+  col.forEach(item => {
+    //window.mainContainer.appendChild(item);
+    $('.main-content').append(item);
+  });
+}
+
+async function createTop5Elo(players){
+  //console.log(players);
+
+  let mainDiv = document.querySelector(".top-main.topElo");
+
+  for (let i = 0; i < 5; i++) {
+    let playerContainer = document.createElement("a");
+    playerContainer.classList.add("player-container");
+
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+
+    let img = document.createElement("img");
+    img.src = "";
+
+    imgContainer.appendChild(img);
+    playerContainer.appendChild(imgContainer);
+
+    let valuesContainer = document.createElement("div");
+    valuesContainer.classList.add("values");
+
+    let name = document.createElement("span");
+    name.classList.add("name");
+    name.innerHTML = "Nome";
+
+    valuesContainer.appendChild(name);
+
+    name = document.createElement("span");
+    name.innerHTML = "Elo";
+    valuesContainer.appendChild(name);
+
+    name = document.createElement("span");
+    name.innerHTML = "K/D Ratio";
+    valuesContainer.appendChild(name);
+
+    playerContainer.appendChild(valuesContainer);
+    mainDiv.appendChild(playerContainer);
+  }
+
+  return mainDiv;
+}
+
+async function createTop5Kd(players){
+  //console.log(players);
+
+  let mainDiv = document.querySelector(".top-main.topKd");
+
+  for (let i = 0; i < 5; i++) {
+    let playerContainer = document.createElement("a");
+    playerContainer.classList.add("player-container");
+
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+
+    let img = document.createElement("img");
+    img.src = "";
+
+    imgContainer.appendChild(img);
+    playerContainer.appendChild(imgContainer);
+
+    let valuesContainer = document.createElement("div");
+    valuesContainer.classList.add("values");
+
+    let name = document.createElement("span");
+    name.classList.add("name");
+    name.innerHTML = "Nome";
+
+    valuesContainer.appendChild(name);
+
+    name = document.createElement("span");
+    name.innerHTML = "K/D Ratio";
+    valuesContainer.appendChild(name);
+
+    name = document.createElement("span");
+    name.innerHTML = "Elo";
+    valuesContainer.appendChild(name);
+
+    playerContainer.appendChild(valuesContainer);
+    mainDiv.appendChild(playerContainer);
+  }
+
+  return mainDiv;
 }
